2076079906513bfa006ff9c23a739657bebdb324
Piotr Kamiński <pk429300@students.mimuw.edu.pl>
2022-05-18 10:23:41 +0200

    working boost

--- main.cpp
+++ main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
+#include <boost/asio.hpp>
+#include <boost/program_options.hpp>
 
-//#include <boost/asio.hpp>
-//#include <boost/program_options.hpp>
-//namespace po = boost::program_options;
+namespace po = boost::program_options;
 
 int main(int argc, char *argv[]) {
 
-//    po::options_description description("MyTool Usage");
-    //description.add_options()
-    //        ("help", "Display this help message")
-    //        ("version", "Display the version number");
+    po::options_description description("MyTool Usage");
+    description.add_options()
+            ("help", "Display this help message")
+            ("version", "Display the version number");
 
-    //po::variables_map vm;
-    //po::store(po::command_line_parser(argc, argv).options(description).run(), vm);
-    //po::notify(vm);
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv).options(description).run(), vm);
+    po::notify(vm);
 
     std::cout << "Hello, World!" << std::endl;
 

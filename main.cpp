fc241edc6cef498b8c3a8587d32dafdd9a0f6992
Piotr Kamiński <67481570+Laplasjan107@users.noreply.github.com>
2022-05-18 12:22:43 +0200

    Added client constructor

--- main.cpp
+++ main.cpp
@@ -5,27 +5,33 @@
 
 namespace po = boost::program_options;
 
-class ClientOptions {
+struct ClientOptions {
     std::string clientName;
     uint16_t port;
     std::string serverAddress;
     std::string displayAddress;
+
+    ClientOptions(int argumentsCount, char *argumentsTable[]) {
+        po::options_description description("Options parser");
+        description.add_options()
+                ("display-address,d", po::value<int>(), "Port number")
+                ("help,h", "Display this help message")
+                ("player-name,n", "Display this help message")
+                ("port,p", po::value<int>(), "Port number")
+                ("server-address,s", po::value<std::string>(), "Server address");
+
+        po::variables_map programVariables;
+        po::store(po::command_line_parser(argumentsCount, argumentsTable).options(description).run(), programVariables);
+        po::notify(programVariables);
+
+        if (programVariables.count("port")){
+            std::cerr << "Port number " << programVariables["port"].as<int>() << std::endl;
+        }
+    }
 };
 
 int main(int argc, char *argv[]) {
-
-    po::options_description description("MyTool Usage");
-    description.add_options()
-            ("help,h", "Display this help message")
-            ("port,p", po::value<int>(), "Port number")
-            ("version", "Display the version number");
-    po::variables_map vm;
-    po::store(po::command_line_parser(argc, argv).options(description).run(), vm);
-    po::notify(vm);
-
-    if(vm.count("port")){
-        std::cout << "Port number " << vm["port"].as<int>() << std::endl;
-    }
+    ClientOptions client {argc, argv};
 
     std::cout << "Hello, World!" << std::endl;
 
